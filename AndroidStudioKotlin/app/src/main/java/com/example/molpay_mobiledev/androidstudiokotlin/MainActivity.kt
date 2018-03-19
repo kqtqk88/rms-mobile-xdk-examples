1b9f907916d8d716e46e0b96bc4cb750db8c1b08
MOLPay-MobileDev <izzaty@molpay.com>
2018-03-19 12:15:14 +0800

    Update Kotlin syntax

--- AndroidStudioKotlin/app/src/main/java/com/example/molpay_mobiledev/androidstudiokotlin/MainActivity.kt
+++ AndroidStudioKotlin/app/src/main/java/com/example/molpay_mobiledev/androidstudiokotlin/MainActivity.kt
@@ -21,46 +21,46 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
 
             val paymentDetails = HashMap<String, Any>()
-            paymentDetails.put(MOLPayActivity.mp_amount, "1.10")
-            paymentDetails.put(MOLPayActivity.mp_username, "molpayxdk")
-            paymentDetails.put(MOLPayActivity.mp_password, "cT54#Lk@22")
-            paymentDetails.put(MOLPayActivity.mp_merchant_ID, "molpayxdk")
-            paymentDetails.put(MOLPayActivity.mp_app_name, "molpayxdk")
-            paymentDetails.put(MOLPayActivity.mp_order_ID, "3-12-0-004")
-            paymentDetails.put(MOLPayActivity.mp_currency, "MYR")
-            paymentDetails.put(MOLPayActivity.mp_country, "MY")
-            paymentDetails.put(MOLPayActivity.mp_verification_key, "4445db44bdb60687a8e7f7903a59c3a9")
-//            paymentDetails.put(MOLPayActivity.mp_channel, "");
-            paymentDetails.put(MOLPayActivity.mp_bill_description, "Cordova payment test")
-            paymentDetails.put(MOLPayActivity.mp_bill_name, "Developer")
-            paymentDetails.put(MOLPayActivity.mp_bill_email, "clewlb@gmail.com")
-            paymentDetails.put(MOLPayActivity.mp_bill_mobile, "+647452")
+            paymentDetails[MOLPayActivity.mp_amount] = "1.10"
+            paymentDetails[MOLPayActivity.mp_username] = ""
+            paymentDetails[MOLPayActivity.mp_password] = ""
+            paymentDetails[MOLPayActivity.mp_merchant_ID] = ""
+            paymentDetails[MOLPayActivity.mp_app_name] = ""
+            paymentDetails[MOLPayActivity.mp_order_ID] = "M0001"
+            paymentDetails[MOLPayActivity.mp_currency] = "MYR"
+            paymentDetails[MOLPayActivity.mp_country] = "MY"
+            paymentDetails[MOLPayActivity.mp_verification_key] = ""
+//            paymentDetails[MOLPayActivity.mp_channel] = ""
+            paymentDetails[MOLPayActivity.mp_bill_description] = "Test MOLPay"
+            paymentDetails[MOLPayActivity.mp_bill_name] = "molpay"
+            paymentDetails[MOLPayActivity.mp_bill_email] = "email@email.com"
+            paymentDetails[MOLPayActivity.mp_bill_mobile] = ""
 
-            //paymentDetails.put(MOLPayActivity.mp_channel_editing, false);
-            //paymentDetails.put(MOLPayActivity.mp_editing_enabled, false);
-            //paymentDetails.put(MOLPayActivity.mp_transaction_id, "");
-            //paymentDetails.put(MOLPayActivity.mp_request_type, "");
-            //String binlock[] = {"123456","234567"};
-            //paymentDetails.put(MOLPayActivity.mp_bin_lock, binlock);
-            //paymentDetails.put(MOLPayActivity.mp_bin_lock_err_msg, "Wrong BIN format");
-            //paymentDetails.put(MOLPayActivity.mp_is_escrow, "");
-            //paymentDetails.put(MOLPayActivity.mp_filter, "1");
-            //paymentDetails.put(MOLPayActivity.mp_custom_css_url, "file:///android_asset/custom.css");
-            //paymentDetails.put(MOLPayActivity.mp_preferred_token, "");
-            //paymentDetails.put(MOLPayActivity.mp_tcctype, ""); // SALS // AUTH
-            //paymentDetails.put(MOLPayActivity.mp_is_recurring, false);
-            //String allowedchannels[] = {"credit","credit3"};
-            //paymentDetails.put(MOLPayActivity.mp_allowed_channels, allowedchannels);
-            //paymentDetails.put(MOLPayActivity.mp_sandbox_mode, true);
-            //paymentDetails.put(MOLPayActivity.mp_express_mode, true);
-            //paymentDetails.put(MOLPayActivity.mp_advanced_email_validation_enabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_advanced_phone_validation_enabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_bill_name_edit_disabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_bill_email_edit_disabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_bill_mobile_edit_disabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_bill_description_edit_disabled, true);
-            //paymentDetails.put(MOLPayActivity.mp_language, "EN");
-            //paymentDetails.put(MOLPayActivity.mp_dev_mode, false);
+            //paymentDetails[MOLPayActivity.mp_channel_editing] = false
+            //paymentDetails[MOLPayActivity.mp_editing_enabled] = false
+            //paymentDetails[MOLPayActivity.mp_transaction_id] = ""
+            //paymentDetails[MOLPayActivity.mp_request_type] = ""
+            //val binlock = arrayOf("123456","234567")
+            //paymentDetails[MOLPayActivity.mp_bin_lock] = binlock
+            //paymentDetails[MOLPayActivity.mp_bin_lock_err_msg] = "Wrong BIN format"
+            //paymentDetails[MOLPayActivity.mp_is_escrow] = ""
+            //paymentDetails[MOLPayActivity.mp_filter] = "1"
+            //paymentDetails[MOLPayActivity.mp_custom_css_url] = "file:///android_asset/custom.css"
+            //paymentDetails[MOLPayActivity.mp_preferred_token] = ""
+            //paymentDetails[MOLPayActivity.mp_tcctype] = "" // SALS // AUTH
+            //paymentDetails[MOLPayActivity.mp_is_recurring] = false
+            //val allowedchannels = arrayOf("credit","credit3")
+            //paymentDetails[MOLPayActivity.mp_allowed_channels] = allowedchannels
+            //paymentDetails[MOLPayActivity.mp_sandbox_mode] = true
+            //paymentDetails[MOLPayActivity.mp_express_mode] = true
+            //paymentDetails[MOLPayActivity.mp_advanced_email_validation_enabled] = true
+            //paymentDetails[MOLPayActivity.mp_advanced_phone_validation_enabled] = true
+            //paymentDetails[MOLPayActivity.mp_bill_name_edit_disabled] = true
+            //paymentDetails[MOLPayActivity.mp_bill_email_edit_disabled] = true
+            //paymentDetails[MOLPayActivity.mp_bill_mobile_edit_disabled] = true
+            //paymentDetails[MOLPayActivity.mp_bill_description_edit_disabled] = true
+            //paymentDetails[MOLPayActivity.mp_language]="EN"
+            //paymentDetails[MOLPayActivity.mp_dev_mode]=false
 
             val intent = Intent(this@MainActivity, MOLPayActivity::class.java)
             intent.putExtra(MOLPayActivity.MOLPayPaymentDetails, paymentDetails)
