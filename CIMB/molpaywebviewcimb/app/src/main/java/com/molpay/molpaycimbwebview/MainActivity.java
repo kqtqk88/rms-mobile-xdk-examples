d20278b6844b82247e37090396e78eccd2778f6a
Leow <lelouchliew93@hotmail.com>
2018-04-03 17:28:11 +0900

    fixed cimb webview

--- CIMB/molpaywebviewcimb/app/src/main/java/com/molpay/molpaycimbwebview/MainActivity.java
+++ CIMB/molpaywebviewcimb/app/src/main/java/com/molpay/molpaycimbwebview/MainActivity.java
@@ -12,14 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        
+        
         wv1=(WebView)findViewById(R.id.webView);
         wv2=(WebView)findViewById(R.id.webView2);
         wv2.setVisibility(View.GONE);
-
+        
         String url = "https://www.molpay.com/seamless-demo-v3.16/index.html";
-//        String url = "https://www.onlinepayment.com.my/MOLPay/pay/molpay/cimb.php?amount=1.10&orderid=DEMO9521&bill_desc=testing+by+MOLPay&bill_name=MOLPay+Demo&bill_email=demo@molpay.com&bill_mobile=55218438&currency=MYR";
+        //        String url = "https://www.onlinepayment.com.my/MOLPay/pay/molpay/cimb.php?amount=1.10&orderid=DEMO9521&bill_desc=testing+by+MOLPay&bill_name=MOLPay+Demo&bill_email=demo@molpay.com&bill_mobile=55218438&currency=MYR";
         wv1.getSettings().setJavaScriptEnabled(true);
         wv1.getSettings().setDomStorageEnabled(true);
         wv1.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         wv2.getSettings().setJavaScriptEnabled(true);
         wv2.getSettings().setDomStorageEnabled(true);
         wv2.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        wv2.getSettings().setSupportMultipleWindows(true);
         wv1.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -36,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        wv2.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return true;
+            }
+        });
         wv1.loadUrl(url);
     }
 }
+
