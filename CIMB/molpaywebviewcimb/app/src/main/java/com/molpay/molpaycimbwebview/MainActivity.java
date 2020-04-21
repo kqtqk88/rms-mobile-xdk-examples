9fd7eb148b0b275cda54fcf0ad8eb70c7f4f4fac
hisyam <hisyam@molpay.com>
2020-04-21 17:42:28 +0800

    Added ionic 4 xdk example project

--- CIMB/molpaywebviewcimb/app/src/main/java/com/molpay/molpaycimbwebview/MainActivity.java
+++ CIMB/molpaywebviewcimb/app/src/main/java/com/molpay/molpaycimbwebview/MainActivity.java
@@ -18,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
         wv2=(WebView)findViewById(R.id.webView2);
         wv2.setVisibility(View.GONE);
         
-        String url = "https://www.molpay.com/seamless-demo-v3.16/index.html";
-        //        String url = "https://www.onlinepayment.com.my/MOLPay/pay/molpay/cimb.php?amount=1.10&orderid=DEMO9521&bill_desc=testing+by+MOLPay&bill_name=MOLPay+Demo&bill_email=demo@molpay.com&bill_mobile=55218438&currency=MYR";
+        //String url = "https://www.merchant.razer.com/seamless-demo-v3.16/index.html";
+        //String url = "https://www.onlinepayment.com.my/MOLPay/pay/molpay/MB2U.php?amount=1.10&orderid=DEV_HAFEEZ_2243&currency=MYR&bill_name=MOLPay+Demo&bill_email=demo@molpay.com&bill_mobile=55218438&bill_desc=testing+by+MOLPay";
+        String url = "https://www.onlinepayment.com.my/MOLPay/pay/molpay/CIMBCLICKS.php?amount=1.10&orderid=DEV_HAFEEZ_2243&currency=MYR&bill_name=MOLPay+Demo&bill_email=demo@molpay.com&bill_mobile=55218438&bill_desc=testing+by+MOLPay";
         wv1.getSettings().setJavaScriptEnabled(true);
         wv1.getSettings().setDomStorageEnabled(true);
         wv1.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
